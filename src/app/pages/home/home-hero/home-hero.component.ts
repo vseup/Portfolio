0726539735bc19b5632127f1b9e830af4e9bd240
vseup <hello@verenapues.com>
2026-05-14 19:22:32 +0200

    home: added hero section avatar image

--- src/app/pages/home/home-hero/home-hero.component.ts
+++ src/app/pages/home/home-hero/home-hero.component.ts
@@ -1,10 +1,11 @@
+import { NgOptimizedImage } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { ButtonComponent } from '../../../shared/button/button.component';
 
 @Component({
   selector: 'app-home-hero',
-  imports: [RouterLink, ButtonComponent],
+  imports: [NgOptimizedImage, RouterLink, ButtonComponent],
   templateUrl: './home-hero.component.html',
   styleUrl: './home-hero.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
