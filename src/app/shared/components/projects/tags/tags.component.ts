0ff6e00184c225678cb769304f3008805e611ea5
vseup <hello@verenapues.com>
2026-05-14 21:26:21 +0200

    shared/components: added project tags component

---
+++ src/app/shared/components/projects/tags/tags.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import type { ProjectData } from '../../../types/project-data.type';
+
+@Component({
+  selector: 'app-tags',
+  templateUrl: './tags.component.html',
+  styleUrl: './tags.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TagsComponent {
+  readonly title = input.required<ProjectData['title']>();
+  readonly tags = input.required<ProjectData['tags']>();
+}
