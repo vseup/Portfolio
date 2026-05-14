c66f5a280ae2ce73c16f549948960b442f424847
vseup <hello@verenapues.com>
2026-05-14 15:23:44 +0200

    added: extracted page footer component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { PageFooterComponent } from './shared/layout/page-footer/page-footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, PageFooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class App {}
+export class App { }
---
+++ src/app/shared/layout/page-footer/page-footer.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-footer',
+  templateUrl: './page-footer.component.html',
+  styleUrl: './page-footer.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class PageFooterComponent {}
