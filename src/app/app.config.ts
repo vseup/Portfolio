161ee8c931d5b4c21604deb0d052d1704a10f7f6
vseup <hello@verenapues.com>
2026-05-29 17:31:06 +0200

    routing: enable anchor scrolling

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -10,6 +10,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(
       routes,
       withInMemoryScrolling({
+        anchorScrolling: 'enabled',
         scrollPositionRestoration: 'top',
       }),
     ),
