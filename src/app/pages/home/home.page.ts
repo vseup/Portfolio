7a93518b5e356cc40e8a6ca8bdf8cb42d9d70861
vseup <hello@verenapues.com>
2026-05-31 16:36:44 +0200

    home: used divider and fixed header z-index

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { HomeHeroComponent } from '@app/components/home/home-hero/home-hero.component';
 import { ProjectCardComponent } from '@app/components/home/project-card/project-card.component';
+import { DividerComponent } from '@app/components/shared/layout/divider/divider.component';
 import { PROJECTS } from '@app/constants/projects.constants';
 
 @Component({
   selector: 'app-home-page',
-  imports: [HomeHeroComponent, ProjectCardComponent],
+  imports: [HomeHeroComponent, DividerComponent, ProjectCardComponent],
   templateUrl: './home.page.html',
   styleUrl: './home.page.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
