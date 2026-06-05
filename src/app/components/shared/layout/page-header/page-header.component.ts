0bd9557043779a1144daf3695283424829431e16
vseup <hello@verenapues.com>
2026-06-05 08:49:21 +0200

    layout: header - highlight selected link visually

--- src/app/components/shared/layout/page-header/page-header.component.ts
+++ src/app/components/shared/layout/page-header/page-header.component.ts
@@ -1,11 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-page-header',
   templateUrl: './page-header.component.html',
   styleUrl: './page-header.component.scss',
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PageHeaderComponent {}
